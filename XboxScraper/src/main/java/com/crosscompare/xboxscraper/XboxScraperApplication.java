54e924e63b457e8d0d354384212dc0ec1668fac1
RachidChaibugr <rachidchaib@correo.ugr.es>
2025-04-29 00:38:28 +0200

    Creacion XboxScraper

---
+++ XboxScraper/src/main/java/com/crosscompare/xboxscraper/XboxScraperApplication.java
@@ -0,0 +1,13 @@
+package com.crosscompare.xboxscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XboxScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XboxScraperApplication.class, args);
+    }
+
+}
---
+++ XboxScraper/src/test/java/com/crosscompare/xboxscraper/XboxScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crosscompare.xboxscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XboxScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
