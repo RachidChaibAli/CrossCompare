ec9befc2fd45410d236b6d17904049fa1846ae5a
RachidChaibugr <rachidchaib@correo.ugr.es>
2025-04-29 00:36:32 +0200

    Creacion PlaystationScraper

---
+++ PlaystationScraper/src/main/java/com/crosscompare/playstationscraper/PlaystationScraperApplication.java
@@ -0,0 +1,13 @@
+package com.crosscompare.playstationscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaystationScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaystationScraperApplication.class, args);
+    }
+
+}
---
+++ PlaystationScraper/src/test/java/com/crosscompare/playstationscraper/PlaystationScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crosscompare.playstationscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaystationScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
