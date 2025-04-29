eb7190a595a52a8a32bfe0e5cd91b589e49998a9
RachidChaibugr <rachidchaib@correo.ugr.es>
2025-04-29 12:18:07 +0200

    Servicios dockerizados

---
+++ API-Gateway/src/test/java/com/crosscompare/unificador/UnificadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crosscompare.unificador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnificadorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
