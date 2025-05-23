995d94839d1a639a8ee0ff321a07501f3aa46ea5
RachidChaibugr <rachidchaib@correo.ugr.es>
2025-05-23 13:42:39 +0200

    Eureka y config server terminados

--- Unificador/src/main/java/com/crosscompare/unificador/RestClientConfig.java
+++ Unificador/src/main/java/com/crosscompare/unificador/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.crosscompare.unificador;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,23 +8,26 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
     @Bean
+    @LoadBalanced
     public RestClient playstationRestClient() {
         return RestClient.builder()
-                .baseUrl("http://playstation-scraper:8080") // Nombre del servicio y puerto interno en Docker Compose
+                .baseUrl("http://playstation-scraper") // Nombre del servicio y puerto interno en Docker Compose
                 .build();
     }
 
     @Bean
+    @LoadBalanced
     public RestClient xboxRestClient() {
         return RestClient.builder()
-                .baseUrl("http://xbox-scraper:8080") // Nombre del servicio y puerto interno en Docker Compose
+                .baseUrl("http://xbox-scraper") // Nombre del servicio y puerto interno en Docker Compose
                 .build();
     }
 
     @Bean
+    @LoadBalanced
     public RestClient steamRestClient() {
         return RestClient.builder()
-                .baseUrl("http://steam-api:8080") // Nombre del servicio y puerto interno en Docker Compose
+                .baseUrl("http://steam-api") // Nombre del servicio y puerto interno en Docker Compose
                 .build();
     }
 }
